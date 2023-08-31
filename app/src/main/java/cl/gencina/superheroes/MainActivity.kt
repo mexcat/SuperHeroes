4cbbcb0c8ac7de40aec9ed8f47b86892f7f23d6f
German Encina <gencina2@gmail.com>
2023-08-31 19:26:23 -0400

    Initial commit

---
+++ app/src/main/java/cl/gencina/superheroes/MainActivity.kt
@@ -0,0 +1,11 @@
+package cl.gencina.superheroes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
